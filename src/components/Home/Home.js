4dc024cf6ea2083f6be54990e4b1d09c921e7741
Shamim <yeasinshamim7@gmail.com>
2022-04-28 12:22:16 +0600

    service loaded successfully

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import SingleService from '../SingleService/SingleService';
 
 const Home = () => {
     const [volunteerServices, setVolunteerServices] = useState([]);
@@ -16,8 +17,12 @@ const Home = () => {
 
 
     return (
-        <div>
-
+        <div >
+            <div className='grid md:grid-cols-4 gap-5 px-20 py-5'>
+                {
+                    volunteerServices.map(service => <SingleService key={service._id} service={service}></SingleService>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const SingleService = ({ service }) => {
-    const { img } = service;
+    const { img, title } = service;
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className='relative'>
+            <img className='rounded' src={img} alt="" />
+            <h5 className='absolute bottom-0 w-full text-center py-2 text-white font-bold cursor-pointer rounded-b bg-yellow-300'>{title}</h5>
         </div>
     );
 };
